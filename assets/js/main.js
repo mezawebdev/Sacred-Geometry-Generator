781612a68e91322fb3a48fc9482631b437b61dcc
Alex Meza <mezawebdev@gmail.com>
2017-08-02 18:43:44 -0700

    Fixed jquery io draggable method on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,36 @@
 $(document).ready(function() {
 //----------------
-//  Menu Handlers
+//  Editor Menu
 //----------------
 var menuMinimized = false;
+var canOpenWindow = false;
+var hasDisplayedMessage = false;
 
-// 1. Minimize button
-$(".minimize").on("click", function(button) {
-	$("#overlay-message").fadeIn();
+// 1.- Close button
+$(".close-button").on("click", function(button) {
+	if (! hasDisplayedMessage) {
+		$("#overlay-message").fadeIn();
+	}
 	setTimeout(function() {
 		$("#overlay-message").fadeOut();
-	}, 3000);
-	$(".menu").animate({
-		height: 0,
-		padding: 0
-	});
+		canOpenWindow = true;
+	}, 2000);
+	$(".menu").fadeOut();
 	$("#overlay-button").css("pointer-events", "auto");
 	menuMinimized = true;
+	hasDisplayedMessage = true;
 });
 
+// 1.-- Show overlay after closing editor window
 $("#overlay-button").on("click", function() {
-	if (menuMinimized) {
-		$(".menu").animate({
-			height: "290px",
-			padding: "20px"
-		});
+	if (menuMinimized && canOpenWindow) {
+		$(".menu").fadeIn();
 		$("#overlay-button").css("pointer-events", "none");
 	}
 });
 
+// 2.- Make menu draggable
+$(".menu").draggable();
+
+//----------------- The End -----------------//
 });
