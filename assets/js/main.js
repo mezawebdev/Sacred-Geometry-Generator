eecf1452b1f3ffca7cd909fd0414caa07da76824
Alex Meza <mezawebdev@gmail.com>
2017-08-17 04:49:45 -0700

    Fixed bugs on bg speed slider, still buggy tho

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,54 +24,51 @@ $(document).ready(function() {
 		return slider.noUiSlider.get();
 	}
 
-	//----------------------
-	// Geometry Objects
-	//----------------------
-	var geomLayer8 = $(".eigth-layer .layer-1, .eigth-layer .layer-2");
-	var geomLayer7 = $(".seventh-layer .layer-1, .seventh-layer .layer-2");
-	var geomLayer6 = $(".sixth-layer .layer-1, .sixth-layer .layer-2");
-	var geomLayer5 = $(".fifth-layer .layer-1, .fifth-layer .layer-2");
-	var geomLayer4 = $(".fourth-layer .layer-1, .fourth-layer .layer-2");
-	var geomLayer3 = $(".third-layer .layer-1, .third-layer .layer-2");
-	var geomLayer2 = $(".second-layer .layer-1, .second-layer .layer-2");
-	var geomLayer1 = $(".first-layer .layer-1, .first-layer .layer-2");
-	var geomLayer0 = $(".zero-layer .layer-1, .zero-layer .layer-2");
-	var geomLayer01 = $(".negative-layer-1 .layer-1, .negative-layer-1 .layer-2");
-	var geomLayer02 = $(".negative-layer-2 .layer-1, .negative-layer-2 .layer-2");
-
 	function setShapeSpeed(value) {
 		var maxDuration = 180;
 		var updatedDuration = maxDuration - value;
 		console.log("Max Duration: " + maxDuration);
 		console.log("Current Duration: " + updatedDuration);
-		$(geomLayer8).css("animation-duration", updatedDuration + "s");
-		$(geomLayer7).css("animation-duration", updatedDuration + "s");
-		$(geomLayer6).css("animation-duration", updatedDuration + "s");
-		$(geomLayer5).css("animation-duration", updatedDuration + "s");
-		$(geomLayer4).css("animation-duration", updatedDuration + "s");
-		$(geomLayer3).css("animation-duration", updatedDuration + "s");
-		$(geomLayer2).css("animation-duration", updatedDuration + "s");
-		$(geomLayer1).css("animation-duration", updatedDuration + "s");
-		$(geomLayer0).css("animation-duration", updatedDuration + "s");
-		$(geomLayer01).css("animation-duration", updatedDuration + "s");
-		$(geomLayer02).css("animation-duration", updatedDuration + "s");
+		$(".eigth-layer .layer-1, .eigth-layer .layer-2").css("animation-duration", updatedDuration + "s");
+		$(".seventh-layer .layer-1, .seventh-layer .layer-2").css("animation-duration", updatedDuration + "s");
+		$(".sixth-layer .layer-1, .sixth-layer .layer-2").css("animation-duration", updatedDuration + "s");
+		$(".fifth-layer .layer-1, .fifth-layer .layer-2").css("animation-duration", updatedDuration + "s");
+		$(".fourth-layer .layer-1, .fourth-layer .layer-2").css("animation-duration", updatedDuration + "s");
+		$(".third-layer .layer-1, .third-layer .layer-2").css("animation-duration", updatedDuration + "s");
+		$(".second-layer .layer-1, .second-layer .layer-2").css("animation-duration", updatedDuration + "s");
+		$(".first-layer .layer-1, .first-layer .layer-2").css("animation-duration", updatedDuration + "s");
+		$(".zero-layer .layer-1, .zero-layer .layer-2").css("animation-duration", updatedDuration + "s");
+		$(".negative-layer-1 .layer-1, .negative-layer-1 .layer-2").css("animation-duration", updatedDuration + "s");
+		$(".negative-layer-2 .layer-1, .negative-layer-2 .layer-2").css("animation-duration", updatedDuration + "s");
 	}
 
-	function updateGradient(color1, color2, color3, color4) {
-		granimInstance.pause();
-		granimInstance.changeState("new-state");
-		granimInstance.play();
+	function updateGradient(color1, color2, color3, color4, speed) {
+		var maxSpeed = 20200;
+		var ran = Math.floor(Math.random() * 10000);
+		console.log("ran: " + ran);
+		var updatedSpeed = Math.floor(maxSpeed - speed);
+		console.log("Set Speed: " + updatedSpeed);
+		granimInstance.states["default-state"] = {
+			gradients: [
+		                [color1, color2],
+		                [color3, color4]
+		            ],
+		            transitionSpeed: ran
+		}
 	}
 
-	//----------------------
-	// Background Gradient
-	//----------------------
+
+
+	//---------------------------------
+	// 			BACKGROUND
+	//---------------------------------
+	//* Variables
 	var gradientColor1 = "#C16B08";
 	var gradientColor2 = "#C1B702";
 	var gradientColor3 = "#20C199";
 	var gradientColor4 = "#C11E27";
+	var bgColor = $("#background-color-picker").attr("value");
 	var backgroundGradientSpeed = 1500;
-
 	var granimInstance = new Granim({
 	    element: '#background-gradient',
 	    name: 'radial-gradient',
@@ -85,33 +82,52 @@ $(document).ready(function() {
 	                [gradientColor3, gradientColor4]
 	            ],
 	            transitionSpeed: backgroundGradientSpeed
-	        },
-	        "new-state": {
-	            gradients: [
-	                [gradientColor1, gradientColor2],
-	                [gradientColor3, gradientColor4]
-	            ],
-	            transitionSpeed: backgroundGradientSpeed
 	        }
 	    }
 	});
-	/*
+
+
+	//* Background Speed
+	var bgSpeedSlider = document.getElementById("background-speed-draggable");
+	noUiSlider.create(bgSpeedSlider, {
+		start: [10000],
+		behaviour: "tap-drag",
+		range: {
+			"min": [0],
+			"max": [20000]
+		}
+	});
+
+	bgSpeedSlider.noUiSlider.on("set", function() {
+		console.log(getSliderValue(bgSpeedSlider));
+		updateGradient(gradientColor1, gradientColor2, gradientColor3, gradientColor4, getSliderValue(bgSpeedSlider));
+	});
+
+
+	updateGradient(gradientColor1, gradientColor2, gradientColor3, gradientColor4, getSliderValue(bgSpeedSlider));
+
+	//* Background Type Button
+	$(".background-block-1").on("click", function(element) {
+		$("#background-gradient").css("opacity", "0");
+		$(".background-gradient-picker-group").css("display", "none");
+		$("#background-color-picker").css("display", "block");
+		$(".background-block-group .col-xs-6 button.active").removeClass("active");
+		$(".background-block-1").toggleClass("active");
+	});
+
+	$(".background-block-2").on("click", function(element) {
+		$(".background-gradient-picker-group").css("display", "block");
+		$("#background-color-picker").css("display", "none");
+		$("#background-gradient").css("opacity", "1");
+		$(".background-block-group .col-xs-6 button.active").removeClass("active");
+		$(".background-block-2").toggleClass("active");
+	});
+
+	//5. Background Solid Color Picker
 	setInterval(function() {
-		var colorValue1 = "#" + $(".background-gradient-picker-1").attr("value");
-		var colorValue2 = "#" + $(".background-gradient-picker-2").attr("value");
-		var colorValue3 = "#" + $(".background-gradient-picker-3").attr("value");
-		var colorValue4 = "#" + $(".background-gradient-picker-4").attr("value");
-		gradientColor1 = colorValue1;
-		gradientColor2 = colorValue2;
-		gradientColor3 = colorValue3;
-		gradientColor4 = colorValue4;
-		updateGradient(gradientColor1, gradientColor2, gradientColor3, gradientColor4);
-		console.log("gradient1: " + gradientColor1);
-		console.log("gradient2: " + gradientColor2);
-		console.log("gradient3: " + gradientColor3);
-		console.log("gradient4: " + gradientColor4);
+		bgColor = $("#background-color-picker").css("background-color");
+		$("body").css("background", "linear-gradient(0deg, rgba(0, 0, 0, 1), " + bgColor + ")");
 	}, 60);
-	*/
 
 	//----------------
 	//  Editor Menu
@@ -180,21 +196,7 @@ $(document).ready(function() {
 		$(".shape-block-4").toggleClass("active");
 	});
 
-	// 4.- Background Type Buttons
-	$(".background-block-1").on("click", function(element) {
-		$("#background-gradient").css("opacity", "0");
-		$(".background-gradient-picker-group").css("display", "none");
-		$("#background-color-picker").css("display", "block");
-		$(".background-block-group .col-xs-6 button.active").removeClass("active");
-		$(".background-block-1").toggleClass("active");
-	});
-	$(".background-block-2").on("click", function(element) {
-		$(".background-gradient-picker-group").css("display", "block");
-		$("#background-color-picker").css("display", "none");
-		$("#background-gradient").css("opacity", "1");
-		$(".background-block-group .col-xs-6 button.active").removeClass("active");
-		$(".background-block-2").toggleClass("active");
-	});
+	
 
 	//4. Speed Range Sliders
 		//--- Create Shape Slider 
@@ -208,36 +210,13 @@ $(document).ready(function() {
 			}
 		});
 		// Set Speeds
-		shapeSpeedSlider.noUiSlider.on("set", function() {
-			setShapeSpeed(getSliderValue(shapeSpeedSlider));
-		});
-
 		shapeSpeedSlider.noUiSlider.on("update", function() {
 			setShapeSpeed(getSliderValue(shapeSpeedSlider));
 		});
 
-		shapeSpeedSlider.noUiSlider.on("start", function() {
-			setShapeSpeed(getSliderValue(shapeSpeedSlider));
-		});
 
 
-		//--- Background speed
-		var bgSpeedSlider = document.getElementById("background-speed-draggable");
-		noUiSlider.create(bgSpeedSlider, {
-			start: [75],
-			behaviour: "tap-drag",
-			range: {
-				"min": [0],
-				"max": [150]
-			}
-		});
-
-	//5. Background Color Picker
-	var bgColor = $("#background-color-picker").attr("value");
-	setInterval(function() {
-		bgColor = $("#background-color-picker").css("background-color");
-		$("body").css("background", "linear-gradient(0deg, rgba(0, 0, 0, 1), " + bgColor + ")");
-	}, 60);
+	
 
 
 
